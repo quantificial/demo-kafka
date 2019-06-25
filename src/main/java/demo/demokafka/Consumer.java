dc0c667bea5f0b0dfd1f0938307ed8e5fcd1bb3f
Johnson Fu <johnson.fu@chubb.com>
2019-06-25 17:39:51 +0800

    add all

---
+++ src/main/java/demo/demokafka/Consumer.java
@@ -0,0 +1,17 @@
+package demo.demokafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Consumer {
+	private final Logger logger = LoggerFactory.getLogger(Consumer.class);
+
+	@KafkaListener(topics = "demo", groupId = "group_id")
+	public void consume(String message) {
+		logger.info(String.format("$$ -> Consumed Message -> %s", message));
+	}
+	
+}
